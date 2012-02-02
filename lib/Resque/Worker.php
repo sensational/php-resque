ec5cadba709a6dc3e35a82736e7cae30f1ec7660
Salimane Adjao Moustapha <me@salimane.com>
2012-02-02 15:49:59 +0800

    avoid working with dirty worker ids

--- lib/Resque/Worker.php
+++ lib/Resque/Worker.php
@@ -53,7 +53,7 @@ class Resque_Worker
 	 * @var Resque_Job Current job, if any, being processed by this worker.
 	 */
 	private $currentJob = null;
-	
+
 	/**
 	 * @var int Process ID of child worker processes.
 	 */
@@ -105,7 +105,7 @@ class Resque_Worker
 	 */
 	public static function find($workerId)
 	{
-		if(!self::exists($workerId)) {
+		if(!self::exists($workerId) || false === strpos($workerId, ":")) {
 			return false;
 		}
 
@@ -489,12 +489,14 @@ class Resque_Worker
 		$workerPids = $this->workerPids();
 		$workers = self::all();
 		foreach($workers as $worker) {
-			list($host, $pid, $queues) = explode(':', (string)$worker, 3);
-			if($host != $this->hostname || in_array($pid, $workerPids) || $pid == getmypid()) {
-				continue;
+			if (is_object($worker)) {
+				list($host, $pid, $queues) = explode(':', (string)$worker, 3);
+				if($host != $this->hostname || in_array($pid, $workerPids) || $pid == getmypid()) {
+					continue;
+				}
+				$this->log('Pruning dead worker: ' . (string)$worker, self::LOG_VERBOSE);
+				$worker->unregisterWorker();
 			}
-			$this->log('Pruning dead worker: ' . (string)$worker, self::LOG_VERBOSE);
-			$worker->unregisterWorker();
 		}
 	}
 
